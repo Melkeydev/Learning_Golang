f40bdd7b2fc03e86ea708e597b1ed0d87c5bc4d6
Amokstakov <amoksyak@uoguelph.ca>
2021-10-30 21:00:42 -0700

    finished structured error handling

--- lets-go-further/greenlight/cmd/api/main.go
+++ lets-go-further/greenlight/cmd/api/main.go
@@ -1,113 +1,116 @@
 package main
 
 import (
-  "fmt"
-  "flag"
-  "log"
-  "net/http"
-  "os"
-  "time"
-  "context"
-  "database/sql"
-  _"github.com/lib/pq"
-  "github.com/amokstakov/greenlight/internal/data"
+	"context"
+	"database/sql"
+	"flag"
+	"fmt"
+	"github.com/amokstakov/greenlight/internal/data"
+	"github.com/amokstakov/greenlight/internal/jsonlog"
+	_ "github.com/lib/pq"
+	"net/http"
+	"os"
+	"time"
 )
 
 const version = "1.0.0"
 
 type config struct {
-  port int
-  env string
-  db struct {
-    dsn string
-    maxOpenConns int
-    maxIdleConns int
-    maxIdleTime string
-  }
+	port int
+	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
 }
 
 type application struct {
-  config config
-  logger *log.Logger
-  models data.Models
+	config config
+	logger *jsonlog.Logger
+	models data.Models
 }
 
 func main() {
-  var cfg config
+	var cfg config
 
-  flag.IntVar(&cfg.port, "port", 4000, "API Server Port")
-  flag.StringVar(&cfg.env, "env", "dev", "environment")
-  // This needs to use .env or os
-  flag.StringVar(&cfg.db.dsn, "dsn", "host=localhost user=postgres password=postgres dbname=greenlight port=5432 sslmode=disable", "Database connection string")
+	flag.IntVar(&cfg.port, "port", 4000, "API Server Port")
+	flag.StringVar(&cfg.env, "env", "dev", "environment")
+	// This needs to use .env or os
+	flag.StringVar(&cfg.db.dsn, "dsn", "host=localhost user=postgres password=postgres dbname=greenlight port=5432 sslmode=disable", "Database connection string")
 
-  // DB configs
-  flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-connections", 25, "postgres max open connections")
-  flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-connections", 25, "postgres max idle connections")
-  flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "postgres max connection idle time")
+	// DB configs
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-connections", 25, "postgres max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-connections", 25, "postgres max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "postgres max connection idle time")
 
-  flag.Parse()
+	flag.Parse()
 
-  // Initialise logger
-  logger := log.New(os.Stdout, "", log.Ldate | log.Ltime)
+	// Initialise logger
+	logger := jsonlog.New(os.Stdout, jsonlog.LevelInfo)
 
-  // Declare and connect to the DB
-  db, err := openDB(cfg)
-  if err != nil {
-    logger.Fatal(err)
-  }
+	// Declare and connect to the DB
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.PrintFatal(err, nil)
+	}
 
-  defer db.Close()
+	defer db.Close()
 
-  logger.Printf("database connection established")
+	logger.PrintInfo("database connection established", nil)
 
-  // Declare instance of the application
-  app := &application{
-    config: cfg,
-    logger: logger,
-    models: data.NewModels(db),
-  }
+	// Declare instance of the application
+	app := &application{
+		config: cfg,
+		logger: logger,
+		models: data.NewModels(db),
+	}
 
-  server := &http.Server{
-    Addr: fmt.Sprintf(":%d", cfg.port),
-    Handler: app.routes(),
-    IdleTimeout: time.Minute,
-    ReadTimeout: 30 * time.Second,
-    WriteTimeout: 30 * time.Second,
-  }
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
 
-  // Start the HTTP Server
-  logger.Printf("Starting %s server on %s", cfg.env, server.Addr)
-  err = server.ListenAndServe()
-  logger.Fatal(err)
+	// Start the HTTP Server
+	logger.PrintInfo("Starting server", map[string]string{
+		"addr": server.Addr,
+		"env": cfg.env,
+	})
+	err = server.ListenAndServe()
+	logger.PrintFatal(err, nil)
 }
 
 func openDB(cfg config) (*sql.DB, error) {
-  db, err := sql.Open("postgres", cfg.db.dsn)
-  if err != nil {
-    return nil, err
-  }
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
 
-  // set max number of open connections
-  db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	// set max number of open connections
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
 
-  // set the max number of idle connections
-  db.SetMaxIdleConns(cfg.db.maxIdleConns)
+	// set the max number of idle connections
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
 
-  duration, err := time.ParseDuration(cfg.db.maxIdleTime)
-  if err != nil {
-    return nil, err
-  }
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
 
-  // set the max idle timeout
-  db.SetConnMaxIdleTime(duration)
+	// set the max idle timeout
+	db.SetConnMaxIdleTime(duration)
 
-  ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-  defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-  err = db.PingContext(ctx)
-  if err != nil {
-    return nil, err
-  }
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
 
-  return db, nil
+	return db, nil
 }
--- lets-go-further/greenlight/cmd/api/routes.go
+++ lets-go-further/greenlight/cmd/api/routes.go
@@ -5,7 +5,7 @@ import (
   "github.com/julienschmidt/httprouter"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
   router := httprouter.New()
 
   //Add our custom error handling 
@@ -18,7 +18,5 @@ func (app *application) routes() *httprouter.Router {
   router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
   router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
   router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
-
-
-  return router
+  return app.recoverPanic(router)
 }
